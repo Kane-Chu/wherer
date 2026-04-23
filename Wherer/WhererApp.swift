88d15a311787dc8aea0442fd3e0b6900fbb329d4
楚家明 <jiaming.chu@gingkoo.com>
2026-04-24 00:49:54 +0800

    feat: inject ThemeManager into app root

--- Wherer/WhererApp.swift
+++ Wherer/WhererApp.swift
@@ -3,6 +3,7 @@ import SwiftUI
 @main
 struct WhererApp: App {
     let persistenceController = PersistenceController.shared
+    @StateObject private var themeManager = ThemeManager()
 
     init() {
         UserDefaults.standard.set(["zh-Hans"], forKey: "AppleLanguages")
@@ -12,6 +13,7 @@ struct WhererApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(context: persistenceController.container.viewContext)
+                .environmentObject(themeManager)
         }
     }
 }
