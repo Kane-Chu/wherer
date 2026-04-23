a272f8f0d9ed4234ce35f2d398ad1706a36f8f66
楚家明 <jiaming.chu@gingkoo.com>
2026-04-24 00:55:41 +0800

    chore: mark AppColors as deprecated in favor of ThemeManager

--- Wherer/Models/AppColors.swift
+++ Wherer/Models/AppColors.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+@available(*, deprecated, message: "Use ThemeManager.effectiveColors instead")
 enum AppColors {
     static let primaryGradient = LinearGradient(
         gradient: Gradient(colors: [Color(hex: "#667eea"), Color(hex: "#764ba2")]),
